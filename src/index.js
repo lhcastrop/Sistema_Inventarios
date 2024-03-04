a99cfec22ca05f73bb1f6725673a33492adbe3bc
jairsilva0742 <52806396+jairsilva0742@users.noreply.github.com>
2024-03-04 10:22:28 -0500

    revision Jair

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-export { default as App } from './App';
+
+export * from './styles/breackpoints';
+
+export * from './styles/themes';
+
+export * from './styles/variables';
+
+export { Device } from './styles/breackpoints';
+
+export { Dark, Light } from './styles/themes';
+
+export { v } from './styles/variables';
+
+export { default as App, ThemeContext } from './App';
 export { AuthContextProvider, UserAuth } from './context/AuthContext';
 export {  } from './main';
 export { Home } from './pages/Home';
 export { MyRoutes } from './routers/routes';
 export { supabase } from './supabase/supabase.config';
 export { DataModulosConfiguracion, DesplegableUser, LinksArray, SecondarylinksArray, TemasData, TipoDocData, TipouserData } from './utils/dataEstatica';
-export * from './components/templates/HomeTemplate';
-export * from './styles/breackpoints';
-export * from './styles/themes';
-export * from './styles/variables';
-export { Device } from './styles/breackpoints';
-export { Dark, Light } from './styles/themes';
-export { v } from './styles/variables';
+export {Sidebar} from './components/organismos/sidebar/Sidebar';
+export {} from './components/organismos/sidebar/SidebarCard';
+export {HomeTemplate} from './components/templates/HomeTemplate';
+
