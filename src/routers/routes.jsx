cdbdc1e40328d29ee194fe8fe1d40e86aa8d48be
jairsilva0742 <52806396+jairsilva0742@users.noreply.github.com>
2024-04-13 03:38:44 -0500

    adelanto CRUD_

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,7 +1,16 @@
 import {Routes, Route} from "react-router-dom";
-import {Home, Login, ProtectedRoute, UserAuth} from "../index";
+import {Home, Login, ProtectedRoute, UserAuth, useUsuariosStore} from "../index";
+import { useQuery } from "@tanstack/react-query";
 export function MyRoutes(){
     const { user } = UserAuth();
+    const {mostrarUsuarios}=useUsuariosStore();
+    const {data,isLoading,error}=useQuery({
+      queryKey:["mostrar usuarios"],
+      queryFn:mostrarUsuarios,
+    });
+    if (isLoading){
+      return
+    }
     return (
       <Routes>
         <Route path="/login" element={<Login />} />
